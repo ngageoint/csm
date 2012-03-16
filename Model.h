e7fb0dba743a3db377dd009c747ac7c2eca893cf
Scott Minster <sminster@harris.com>
2012-03-16 14:47:39 +0000

    Initial revision

---
+++ Model.h
@@ -0,0 +1,126 @@
+//#############################################################################
+//
+//    FILENAME:          CSMModel.h
+//
+//    CLASSIFICATION:    Unclassified
+//
+//    DESCRIPTION:
+//
+//    Header for abstract base class that is to provide a common interface from
+//    which all Tactical Sensor Model (CSM) plugin models will inherit.
+//
+//    LIMITATIONS:       None
+//
+//
+//    SOFTWARE HISTORY:
+//     Date          Author   Comment   
+//     -----------   ------   -------
+//     02-Mar-2012   SCM      Initial creation
+//
+//    NOTES:
+//
+//#############################################################################
+
+#ifndef __CSMMODEL_H
+#define __CSMMODEL_H
+
+#include "CSMMisc.h"
+#include "CSMWarning.h"
+#include <vector>
+#include <string>
+
+namespace csm {
+
+class CSM_EXPORT_API Model
+{
+public:
+   Model() {}
+   virtual ~Model() {}
+
+   virtual int getVersion() const = 0;
+      //> This method returns the version of the CSM that the sensor model
+      //  complies with.
+      //<
+   virtual std::string getModelType() const = 0;
+      //> This method returns the type of model implemented by the derived
+      //  class.  Valid values include, but are not limited to: "EO", "SAR",
+      //  and "LIDAR".
+      //<
+   virtual std::string getModelName() const = 0;
+      //> This method returns a string identifying the name of the senor model.
+      //<
+   virtual std::string getPedigree() const = 0;
+      //> The getPedigree() method returns a character std::string that
+      //  identifies the sensor, the model type, its mode of acquisition
+      //  and processing path. For example, an image that could produce
+      //  either an optical sensor model or a cubic rational polynomial
+      //  model would produce different pedigrees for each case.
+      //<
+
+   virtual std::srting getImageIdentifier() const = 0;
+      //> This method returns the unique identifier to indicate the imaging
+      //  operation associated with this sensor model.
+      //
+      //  This method may return an empty string if the ID is unknown.
+      //<
+   virtual void setImageIdentifier(const std::string& imageId,
+                                   WarningList* warnings = NULL) = 0;
+      //> This method sets the unique identifier for the image to which the
+      //  sensor model pertains.
+      //<
+
+   virtual std::string getSensorIdentifier() const = 0;
+      //> The getSensorIdentifier() method returns sensorId to indicate
+      //  which sensor was used to acquire the image. This sensorId is
+      //  meant to uniquely identify the sensor used to make the image.
+      //
+      //  This method may return an empty string if the sensor ID is unknown.
+      //<
+   virtual std::string getPlatformIdentifier() const = 0;
+      //> The getPlatformIdentifier() method returns platformId to indicate
+      //  which platform was used to acquire the image. This platformId
+      //  is meant to uniquely identify the platform used to collect the
+      //  image.
+      //
+      //  This method may return an empty string if the sensor ID is unknown.
+      //<
+   virtual std::string getCollectionIdentifier() const = 0;
+      //> This method returns a unique identifer that uniquely identifies
+      //  a collection activity by a sensor platform. This ID will vary
+      //  depending on the sensor type and platform.
+      //<
+
+   virtual EcefCoord getReferencePoint() const = 0;
+      //> This method returns the ground point indicating the general
+      //  location of the image.
+      //<
+   virtual void setReferencePoint(const EcefCoord& groundPt) = 0;
+      //> This method sets the ground point indicating the general location
+      //  of the image.
+      //<
+
+   virtual ImageCoord getImageSize() const = 0;
+      //> This method returns the number of lines and samples in the imaging
+      //  operation.
+      //
+      //  TODO: should this be on the base class???
+      //<
+
+   //---
+   // Sensor Model State
+   //---
+   virtual std::string getSensorModelState() const = 0;
+      //> This method returns the current state of the model in an
+      //  intermediate form. This intermediate form can then processed,
+      //  for example, by saving to file so that this model
+      //  can be instantiated at a later date. The derived SensorModel
+      //  is responsible for saving all information needed to restore
+      //  itself to its current state from this intermediate form.
+      //  An empty string is returned if it is not possible to save the
+      //  current state.
+      //<
+};
+
+} // namespace csm
+
+#endif
---
+++ SensorTypeAndMode.h
@@ -0,0 +1,65 @@
+//#############################################################################
+//
+//    FILENAME:          CSMSensorTypeAndMode.h
+//
+//    CLASSIFICATION:    Unclassified
+//
+//    DESCRIPTION:
+//
+//    Header for the sensor type and mode information.
+//
+//    LIMITATIONS:       None
+//
+//
+//    SOFTWARE HISTORY:
+//     Date          Author   Comment   
+//     -----------   ------   -------
+//     02-Mar-2012   SCM      Initial version
+//
+//    NOTES:
+//
+//#############################################################################
+
+#ifndef __CSMSENSORTYPEANDMODE_H
+#define __CSMSENSORTYPEANDMODE_H
+
+#include <string>
+
+namespace csm {
+
+class SensorTypeAndMode
+{
+public:
+   SensorTypeAndMode() : theSensorType(), theSensorMode() {}
+   SensorTypeAndMode(const std::string sensorType,
+                     const std::string sensorMode)
+      : theSensorType(sensorType), theSensorMode(sensorMode) {}
+
+   const std::string& type() const {return theSensorType;}
+   const std::string& mode() const {return theSensorMode;}
+
+private:
+   std::string theSensorType;
+   std::string theSensorMode;
+};
+
+#define TYPE_UNK           "UNKNOWN"
+#define TYPE_EO            "EO"
+#define TYPE_IR            "IR"
+#define TYPE_MWIR          "MWIR"
+#define TYPE_LWIR          "LWIR"
+#define TYPE_SAR           "SAR"
+#define TYPE_EOIRSC        "EO_IR_SPECIAL_CASE"
+#define MODE_FRAME         "FRAME"
+#define MODE_PULSE         "PULSE"
+#define MODE_PB            "PUSHBROOM"
+#define MODE_WB            "WHISKBROOM"
+#define MODE_SPOT          "SPOT"
+#define MODE_STRIP         "STRIP"
+#define MODE_SCAN          "SCAN"
+#define MODE_VIDEO         "VIDEO"
+#define MODE_BODY_POINTING "BODY_POINTING"
+
+} // namespace csm
+
+#endif
