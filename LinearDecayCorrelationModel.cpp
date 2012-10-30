a428e6a683d1d17f38d1b84a4ba413c807d5c77e
Scott Minster <sminster@harris.com>
2012-10-30 21:52:18 +0000

    Initial revision

---
+++ LinearDecayCorrelationModel.cpp
@@ -0,0 +1,221 @@
+/**
+ * This file contains the implementation of the
+ * CSMLinearDecayCorrelationModel class.  See the comments in the header file
+ * for more information about the class interface.
+ **/
+#define CSM_LIBRARY
+#include "CSMLinearDecayCorrelationModel.h"
+#include "CSMError.h"
+
+#ifdef IRIXN32
+#include "math.h"
+#else
+#include <cmath>
+using std::exp;
+using std::fabs;
+#endif
+
+namespace csm {
+
+LinearDecayCorrelationModel::LinearDecayCorrelationModel(
+   size_t numSMParams,
+   size_t numCPGroups)
+   :
+   theGroupMapping(numSMParams, -1),
+   theCorrParams(numSMParams)
+{
+   // set the format string in the base class
+   theFormat = "LinearDecayCorrelation";
+}
+
+LinearDecayCorrelationModel::~LinearDecayCorrelationModel()
+{
+}
+
+size_t LinearDecayCorrelationModel::getNumSensorModelParameters() const
+{
+   return theGroupMapping.size();
+}
+
+size_t LinearDecayCorrelationModel::getNumCorrelationParameterGroups() const
+{
+   return theCorrParams.size();
+}
+
+int LinearDecayCorrelationModel::getCorrelationParameterGroup(size_t smParamIndex) const
+{
+   // make sure the index falls within the acceptable range
+   checkSensorModelParameterIndex(smParamIndex, "getCorrelationParameterGroup");
+
+   // return the correlation parameter group index by reference
+   return theGroupMapping[smParamIndex];
+}
+
+void LinearDecayCorrelationModel::setCorrelationParameterGroup(
+   size_t smParamIndex,
+   size_t cpGroupIndex)
+{
+   // make sure the indices fall within the acceptable ranges
+   checkSensorModelParameterIndex(smParamIndex, "setCorrelationParameterGroup");
+   checkParameterGroupIndex(cpGroupIndex, "setCorrelationParameterGroup");
+
+   // set the group index for the given sensor model parameter
+   theGroupMapping[smParamIndex] = cpGroupIndex;
+}
+
+void LinearDecayCorrelationModel::setCorrelationGroupParameters(
+   size_t cpGroupIndex,
+   const std::vector<double> &initialCorrsPerSegment,
+   const std::vector<double> &timesPerSegment)
+{
+   setCorrelationGroupParameters(
+      cpGroupIndex, Parameters(initialCorrsPerSegment, timesPerSegment) );
+}
+
+void LinearDecayCorrelationModel::setCorrelationGroupParameters(
+   size_t            cpGroupIndex,
+   const Parameters& params)
+{
+   static const char* const MODULE =
+      "csm::LinearDecayCorrelationModel::setCorrelationGroupParameters";
+
+   // make sure the index falls within the acceptable range
+   checkParameterGroupIndex(cpGroupIndex, "setCorrelationGroupParameters");
+
+   // make sure the values of each correlation model parameter
+   // fall within acceptable ranges
+   int size = params.theInitialCorrsPerSegment.size();
+   if( size != params.theTimesPerSegment.size() )
+   {
+      throw Error(
+         Error::BOUNDS,
+         "Must have equal number of correlations and times.",
+         MODULE);
+   }
+
+   double corr, prevCorr;
+   double time, prevTime;
+
+   if( size > 1 )
+   {
+      for( int i = 0; i < size; i++ )
+      {
+         corr = params.theInitialCorrsPerSegment[i];
+         time = params.theTimesPerSegment[i];
+         if( corr < 0.0 || corr > 1.0 )
+         {
+            throw Error(
+               Error::BOUNDS,
+               "Correlation must be in range [0..1].",
+               MODULE);
+         }
+
+         if( i > 0 )
+         {
+            prevCorr = params.theInitialCorrsPerSegment[i-1];
+            prevTime = params.theTimesPerSegment[i-1];
+            if( corr > prevCorr )
+            {
+               throw Error(
+                  Error::BOUNDS,
+                  "Correlation must be monotomically decreasing.",
+                  MODULE);
+            }
+            if( time < prevTime )
+            {
+               throw Error(
+                  Error::BOUNDS,
+                  "Time must be monotomically increasing.",
+                  MODULE);
+            }
+         }
+      }
+   }
+
+   // store the correlation parameter values
+   theCorrParams[cpGroupIndex] = params;
+}
+
+double LinearDecayCorrelationModel::getCorrelationCoefficient(
+   size_t cpGroupIndex,
+   double deltaTime) const
+{
+   // make sure the index falls within the acceptable range
+   checkParameterGroupIndex(cpGroupIndex, "getCorrelationCoefficient");
+
+   // compute the value of the correlation coefficient
+   const Parameters& cp = theCorrParams[cpGroupIndex];
+   int size = cp.theInitialCorrsPerSegment.size();
+   double corrCoeff = 0.0;
+
+   double adt = fabs(deltaTime);
+   double prevCorr = cp.theInitialCorrsPerSegment[0];
+   double prevTime = cp.theTimesPerSegment[0];
+
+   double correlation = prevCorr;
+
+   for (int s = 1; s < size; s ++)
+   {
+      double corr = cp.theInitialCorrsPerSegment[s];
+      double time = cp.theTimesPerSegment[s];
+      if (adt <= time)
+      {
+         if (time - prevTime != 0.0)
+         {
+            correlation =
+               prevCorr +
+               (adt - prevTime) / (time - prevTime) * (corr - prevCorr);
+         }
+         break;
+      }
+      prevCorr = corr;
+      prevTime = time;
+      correlation = prevCorr;
+   }
+
+   // if necessary, clamp the coefficient value to the acceptable range
+   if (correlation < 0.0) 
+      correlation =  0.0;
+   if (correlation > 1.0)
+      correlation = 1.0;
+
+   return correlation;
+}
+
+const LinearDecayCorrelationModel::Parameters&
+LinearDecayCorrelationModel::getCorrelationGroupParameters(
+   size_t cpGroupIndex) const
+{
+   // make sure the index falls within the acceptable range
+   checkParameterGroupIndex(cpGroupIndex, "getCorrelationGroupParameters");
+
+   return theCorrParams[cpGroupIndex];
+}
+
+void LinearDecayCorrelationModel::checkSensorModelParameterIndex(
+   size_t             smParamIndex, 
+   const std::string& functionName) const
+{
+   if (smParamIndex >= theGroupMapping.size())
+   {
+      throw Error(
+         Error::INDEX_OUT_OF_RANGE,
+         "Sensor model parameter index is out of range.",
+         "csm::LinearDecayCorrelationModel::" + functionName);
+   }
+}
+
+void LinearDecayCorrelationModel::checkParameterGroupIndex(
+   size_t             groupIndex,
+   const std::string& functionName) const
+{
+   if (groupIndex >= theCorrParams.size())
+   {
+      throw Error(
+         Error::INDEX_OUT_OF_RANGE,
+         "Correlation parameter group index is out of range.",
+         "csm::LinearDecayCorrelationModel::" + functionName);
+   }
+}
+
+} // namespace csm
---
+++ LinearDecayCorrelationModel.h
@@ -0,0 +1,203 @@
+//#############################################################################
+//
+//    FILENAME:          CSMLinearDecayCorrelationModel.h
+//
+//    CLASSIFICATION:    Unclassified
+//
+//    DESCRIPTION:
+//
+//    This header defines the CSMLinearDecayCorrelationModel class.  This
+//    class is used to compute the correlation between adjustable parameters in
+//    a community sensor model (CSM).
+//
+//    The class holds correlation data in the form of two lists. The first is a
+//    list of times that specify the start times of each time segment.
+//    The second list spcifies the correlation at the specified time. Between
+//    specified times the correlation is a linear imterpolation in time of the 
+//    correlation at each end of the time segment.
+//
+//    The class assumes that sensor model parameter are divided into disjoint
+//    groups, such that the correlation coefficient between any two parameters
+//    in the same group is given by the equation above, and the correlation
+//    between two parameters in different groups is 0.0.  This implementation
+//    provides a way to assign sensor model parameters to groups, to set the
+//    values of the correlation parameters for a given group, and to compute
+//    the equation above.
+//
+//    LIMITATIONS:       None
+//
+//
+//    SOFTWARE HISTORY:
+//     Date          Author   Comment
+//     -----------   ------   -------
+//     20-Nov-2012   ISK      Initial Release.
+//
+//    NOTES:
+//
+//#############################################################################
+
+#ifndef _CSM3_LINEAR_DECAY_CORRELATION_MODEL_H_
+#define _CSM3_LINEAR_DECAY_CORRELATION_MODEL_H_
+
+#include "CSMCovarianceModel.h"
+#include "CSMWarning.h"
+
+#include <vector>
+
+namespace csm
+{
+
+class CSM_EXPORT_API LinearDecayCorrelationModel : public CovarianceModel
+{
+public:
+   // represents a set of correlation parameters to be associated with one group
+   struct CSM_EXPORT_API Parameters
+   {
+      Parameters() {}
+      Parameters(
+         const std::vector<double> &initialCorrsPerSegment,
+         const std::vector<double> &timesPerSegment)
+      {}
+
+      std::vector<double> theInitialCorrsPerSegment;
+      std::vector<double> theTimesPerSegment;
+   };
+
+   LinearDecayCorrelationModel(size_t numSMParams, size_t numCPGroups);
+      //> Constructor.  The number of sensor model parameters and correlation
+      //  parameter groups must be provided.
+      //
+      // Preconditions:
+      // * numSMParams must be non-negative
+      // * numCPGroups must be non-negative
+      //<
+
+   virtual ~LinearDecayCorrelationModel();
+
+   virtual size_t getNumSensorModelParameters() const;
+      //> Returns the number of sensor model parameters. The returned value
+      //  will be the same as the value of numSMParams passed to the
+      //  constructor when the object was created.
+      //<
+
+   virtual size_t getNumCorrelationParameterGroups() const;
+      //> Returns the number of correlation parameter groups. The returned
+      //  value will be the same as the value of numCPGroups passed to the
+      //  constructor when the object was created.
+      //<
+
+   virtual int getCorrelationParameterGroup(size_t smParamIndex) const;
+      //> Returns the index of the correlation parameter group to which the
+      //  given sensor model parameter belongs.  The smParamIndex variable is 
+      //  the index of a sensor model parameter.  If the sensor model parameter
+      //  does not belong to a group, the return value will be -1.
+      //
+      //  Precondition:
+      //  * 0 <= smParamIndex < numSMParams
+      //<
+
+   virtual double getCorrelationCoefficient(
+      size_t cpGroupIndex,
+      double deltaTime) const;
+      //>  Computes the correlation coefficient for the given correlation 
+      //  parameter group and delta-time.  The cpGroupIndex variable holds the 
+      //  index of a correlation parameter group.  The deltaTime variable 
+      //  represents the difference in time for the correlation calculation.
+      //
+      //  Preconditions:
+      //  * 0 <= cpGroupIndex < numCPGroups
+      //
+      //  Notes:
+      //
+      //  The deltaTime parameter should be positive, but the function uses the
+      //  absolute value of the variable, so a negative deltaTime is acceptable
+      //  input.
+      //
+      //  If the computed correlation coefficient is outside the range [-1, 1],
+      //  the function will "clamp" the returned value to the nearest number
+      //  within that range.  For example, if the correlation coefficient
+      //  equation evaluates to 1.1 for a given deltaTime,
+      //  the value 1.0 will be returned.
+      //<
+
+   void setCorrelationParameterGroup(
+      size_t smParamIndex,
+      size_t cpGroupIndex);
+      //> Sets the index of the correlation parameter group to which the given
+      //  sensor model parameter belongs.  The smParamIndex variable is the 
+      //  index of a sensor model parameter, and the cpGroupIndex variable is
+      //  the index of the correlation parameter group to which it is added.
+      //
+      //  Preconditions:
+      //  * 0 <= smParamIndex < numSMParams
+      //  * 0 <= cpGroupIndex < numCPGroups
+      //
+      //  Notes:
+      //
+      //  Although the getCorrelationParameterGroup function can return -1 
+      //  as a group index (indicating the group has not been set), 
+      //  it is an error to try to set the group index to -1.
+      //<
+
+   void setCorrelationGroupParameters(
+      size_t cpGroupIndex,
+      const std::vector<double> &initialCorrsPerSegment,
+      const std::vector<double> &timesPerSegment);
+      //> Sets the values of the correlation parameters for a given group.  The
+      //  cpGroupIndex variable holds the index of a correlation parameter group
+      //  and the remaining arguments are the values of the correlation
+      //  parameters in that group.
+      //
+      //  Precondition:
+      //  * 0 <= cpGroupIndex < numCPGroups
+      //  * initialCorrsPerSegment and timesPerSegment are the same size
+      //  * timesPerSegment[i] < timesPerSegment[i+1]
+      //  * initialCorrsPerSegment[i] > initialCorrsPerSegment[i+1]
+      //  * 1 > initialCorrsPerSegment[i] > 0
+      //<
+
+   void setCorrelationGroupParameters(
+      size_t            cpGroupIndex,
+      const Parameters& params);
+      //> Sets the values of the correlation parameters for a given group.
+      //
+      //  Throws an exception if cpGroupIndex or any of the correlation
+      //  parameters is out of range.
+      //<
+
+   const Parameters& getCorrelationGroupParameters(size_t cpGroupIndex) const;
+      //> Returns the values of the correlation parameters for a given group.
+      //
+      //  Throws an exception if cpGroupIndex is out of range.
+      //<
+
+protected:
+
+   std::vector<int> theGroupMapping;
+      //> This data member stores the mapping between sensor model parameter
+      //  indices and correlation parameter group indices.
+      //<
+
+   std::vector<Parameters> theCorrParams;
+      //> This data member stores the values of the correlation parameters for
+      //  each parameter group.
+      //<
+
+   void checkSensorModelParameterIndex(
+      size_t             smParamIndex,
+      const std::string& functionName) const;
+      //> If the given sensor model parameter index is not within the range
+      //  [0, numSMParams), throws a CSMError.
+      //<
+
+   void checkParameterGroupIndex(
+      size_t             groupIndex,
+      const std::string& functionName) const;
+      //> If the given correlation parameter group index is not within the range
+      //  [0, numCPGroups), throws a CSMError.
+      //<
+};
+
+} // namespace csm
+
+#endif
