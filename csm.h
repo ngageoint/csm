c19c9a3c40c38e3e57338fd4fbe8edb23413d7d9
Scott Minster <sminster@harris.com>
2012-12-07 13:00:28 +0000

    //     29-Nov-2012   JPK      Added Parameter and SharingCriteria structs and //                            ParamSet struct. //     06-Dec-2012   JPK      Renamed ParamSet and ParamType to Set and Type //                            and put them in new namespace "param". //                            Implemented additional constructors for //                            ImageCoordCovar and EcefCoordCovar and added //                            new struct EcefLocus.

--- csm.h
+++ csm.h
@@ -20,6 +20,13 @@
 //                            Made the covariances public.
 //     30-Oct-2012   SCM      Renamed to csm.h
 //     31-Oct-2012   SCM      Moved common sensor type and mode defines here.
+//     29-Nov-2012   JPK      Added Parameter and SharingCriteria structs and
+//                            ParamSet struct.
+//     06-Dec-2012   JPK      Renamed ParamSet and ParamType to Set and Type
+//                            and put them in new namespace "param".
+//                            Implemented additional constructors for
+//                            ImageCoordCovar and EcefCoordCovar and added
+//                            new struct EcefLocus.
 //
 //    NOTES:
 //
@@ -29,6 +36,7 @@
 #define __CSM_CSM_H
 
 #include <vector>
+#include <string>
 
 #ifdef _WIN32
 # ifdef CSM_LIBRARY
@@ -37,17 +45,20 @@
 #  define CSM_EXPORT_API __declspec(dllimport)
 # endif
 #elif LINUX_BUILD
-# define CSM_EXPORT_API __atribute__ ((visibility("default")))
+# define CSM_EXPORT_API __attribute__ ((visibility("default")))
 #else
 #  define CSM_EXPORT_API
 #endif
 
-// The getVersion() and getCSMVersion() methods should use CURRENT_CSM_VERSION to
+// The getVersion() and getCsmVersion() methods should use CURRENT_CSM_VERSION to
 // return the CSM API version that the sensor model/plugin was written to.
-#define CURRENT_CSM_VERSION csm::Version(3, 0);
+#define CURRENT_CSM_VERSION csm::Version(3, 0, 1);
+
+// common definition for an unknown name,type,node,etc.
+#define CSM_UNKNOWN "UNKNOWN"
 
 // common sensor types returned from csm::Model::getSensorType()
-#define CSM_SENSOR_TYPE_UNKNOWN       "UNKNOWN"
+#define CSM_SENSOR_TYPE_UNKNOWN       CSM_UNKNOWN
 #define CSM_SENSOR_TYPE_EO            "EO"
 #define CSM_SENSOR_TYPE_IR            "IR"
 #define CSM_SENSOR_TYPE_MWIR          "MWIR"
@@ -56,7 +67,7 @@
 #define CSM_SENSOR_TYPE_EOIRSC        "EO_IR_SPECIAL_CASE"
 
 // common sensor modes returned from csm::Model::getSensorMode()
-#define CSM_SENSOR_MODE_UNKNOWN       "UNKNOWN"
+#define CSM_SENSOR_MODE_UNKNOWN       CSM_UNKNOWN
 #define CSM_SENSOR_MODE_FRAME         "FRAME"
 #define CSM_SENSOR_MODE_PULSE         "PULSE"
 #define CSM_SENSOR_MODE_PB            "PUSHBROOM"
@@ -69,31 +80,96 @@
 
 namespace csm
 {
-   enum ParamType
-   //>
-   // This enumeration lists the possible parameter or characteristic
-   //  types as follows.
-   //
-   //  NONE       - Parameter value has not yet been initialized.
-   //  FICTITIOUS - Parameter value has been calculated by resection
-   //               or other means.
-   //  REAL       - Parameter value has been measured or read from
-   //               support data.
-   //  EXACT      - Parameter value has been specified and is assumed to
-   //               have no uncertainty.
+namespace param
+{
+   enum Type
+      //>
+      // This enumeration lists the possible parameter or characteristic
+      //  types as follows.
+      //
+      //  NONE       - Parameter value has not yet been initialized.
+      //  FICTITIOUS - Parameter value has been calculated by resection
+      //               or other means.
+      //  REAL       - Parameter value has been measured or read from
+      //               support data.
+      //  EXACT      - Parameter value has been specified and is not
+      //               to be adjusted, but may contribute to error
+      //               propagation.
    {
-      NONE,
+      NONE = 0 ,
       FICTITIOUS,
       REAL,
       EXACT
    };
+      //<
+  
+   enum Set
+      //>
+      // This enumeration lists the set of parameters a user may be interested
+      // in exploiting, and membership in one of these sets is determined by
+      //  parameter type.
+      //  VALID      - Parameters of type NONE are excluded. 
+      //               All others are included,
+      //  ADJUSTABLE - Only REAL or FICTICIOUS parameters are included
+      //  FIXED      - Only EXACT parameters are included.
+   {
+      VALID = 0,
+      ADJUSTABLE,
+      FIXED
+   };
+      //<
+}
+   
+   //***
+   // STRUCT: SharingCriteria
+   //> This structure stores information regarding whether or not parameters
+   // can be "shared", based on common characteristics.
    //<
+   //***
+   struct SharingCriteria
+   {
+   public:
+      bool      matchesName;
+      bool      matchesSensorID;
+      bool      matchesPlatformID;
+      bool      matchesCollectionID;
+      bool      matchesTrajectoryID;
+      bool      matchesDateTime;
+      double    maxTimeDelta;
 
+      SharingCriteria()
+         :
+            matchesName         (false),
+            matchesSensorID     (false),
+            matchesPlatformID   (false),
+            matchesCollectionID (false),
+            matchesTrajectoryID (false),
+            matchesDateTime     (false),
+            maxTimeDelta        (0.0)
+      {}
+
+      SharingCriteria(bool   byName,
+                      bool   bySensorID,
+                      bool   byPlatformID,
+                      bool   byCollectionID,
+                      bool   byTrajectoryID,
+                      bool   byDateTime,
+                      double maxDelta)
+         :
+           matchesName         (byName),
+           matchesSensorID     (bySensorID),
+           matchesPlatformID   (byPlatformID),
+           matchesCollectionID (byCollectionID),
+           matchesTrajectoryID (byTrajectoryID),
+           matchesDateTime     (byDateTime),
+           maxTimeDelta        (maxDelta)  
+      {}
+   };
+      
    //***
    // STRUCT: ImageCoord
    //> The ImageCoord structure represents a 2 dimensional point in image space.
-   //  Usually, it represents an abolute point location, but some uses on the
-   //  SensorModel interface use it as a size (location vector).
+   //  Usually, it represents an abolute point location.
    //<
    //***
    struct ImageCoord
@@ -103,6 +179,7 @@ namespace csm
       double samp;
 
       ImageCoord() : line(0.0), samp(0.0) {}
+      
       ImageCoord(double aLine, double aSamp) : line(aLine), samp(aSamp) {}
    };
 
@@ -120,15 +197,52 @@ namespace csm
    {
    public:
       ImageCoordCovar() : ImageCoord() { memset(covariance, 0, sizeof(covariance)); }
+      //> Default Constructor
+      //<
       ImageCoordCovar(double aLine, double aSamp)
          : ImageCoord(aLine, aSamp) { memset(covariance, 0, sizeof(covariance)); }
+      //> Constructor taking point only (all covariance entries are 0)
+      //<    
       ImageCoordCovar(double aLine, double aSamp, double aCovar[4])
          : ImageCoord(aLine, aSamp) { memcpy(covariance, aCovar, sizeof(covariance)); }
+      //> Constructor taking point and a 4 element array of doubles for covariance
+      //  Note that no check is made to insure symmetry of covariance matrix.
+      //< 
+      ImageCoordCovar(double aLine,   double aSamp,
+                      double aCovar00,double aCovar01,
+                                      double aCovar11)
+         :
+            ImageCoord(aLine, aSamp)
+      {
+         covariance[0] = aCovar00;
+         covariance[1] = covariance[2] = aCovar01;
+         covariance[3] = aCovar11;
+      }
+      //> Constructor taking point and upper triangular portion of covariance matrix.
+      //  Elements are assigned to covariance matrix assuming symmetry.
+      //<
 
+      //***
+      //> The 2 x 2 covariance matrix for the image coordinate is stored in a one dimensional
+      // array.  Elements can either be accessed directly from that array, or via a 
+      // two dimensional lookup using the covar2d() methods below.  Note that using the
+      // "non-const" method allows assignment as well as retrieval.
+      // Ex.   To look up the variance in the sample direction of a coordinate "coord" and
+      // assign it to a variable A, the syntax would be :  
+      //
+      // Using one dimensional access : double A = coord.covariance[3];
+      // Using two dimensional access : double A = coord.covar2d(1,1);
+      //
+      // Similarly, to assign a value of 0.5 as the variance in the sample direction 
+      // for "coord", the syntax would be :  
+      //
+      // Using one dimensional access : coord.covariance[3] = 0.5;
+      // Using two dimensional access : coord.covar2d(1,1) = 0.5;
+      //<
+      //***
+      double covariance[4];
       double  covar2d(unsigned int l, unsigned int s) const { return covariance[2*l + s]; }
       double& covar2d(unsigned int l, unsigned int s)       { return covariance[2*l + s]; }
-
-      double covariance[4];
    };
 
    //***
@@ -144,6 +258,7 @@ namespace csm
       double samp;
 
       ImageVector() : line(0.0), samp(0.0) {}
+
       ImageVector(double aLine, double aSamp) : line(aLine), samp(aSamp) {}
    };
 
@@ -166,6 +281,7 @@ namespace csm
       double z;
 
       EcefCoord() : x(0.0), y(0.0), z(0.0) {}
+
       EcefCoord(double aX, double aY, double aZ) : x(aX), y(aY), z(aZ) {}
    };
 
@@ -183,15 +299,57 @@ namespace csm
    {
    public:
       EcefCoordCovar() : EcefCoord() { memset(covariance, 0, sizeof(covariance)); }
+      //> Default Constructor
+      //<   
       EcefCoordCovar(double aX, double aY, double aZ)
          : EcefCoord(aX, aY, aZ) { memset(covariance, 0, sizeof(covariance)); }
+      //> Constructor taking point only (all covariance entries are 0)
+      //<    
       EcefCoordCovar(double aX, double aY, double aZ, double aCovar[9])
          : EcefCoord(aX, aY, aZ) { memcpy(covariance, aCovar, sizeof(covariance)); }
-
-      double  covar2d(unsigned int l, unsigned int s) const { return covariance[3*l + s]; }
-      double& covar2d(unsigned int l, unsigned int s)       { return covariance[3*l + s]; }
-
+      //> Constructor taking point and a 9 element array of doubles for covariance
+      //  Note that no check is made to insure symmetry of covariance matrix.
+      //< 
+      EcefCoordCovar(double aX,       double aY,       double aZ,
+                     double aCovar00, double aCovar01, double aCovar02,
+                                      double aCovar11, double aCovar12,
+                                                       double aCovar22)
+         
+         :
+            EcefCoord(aX, aY, aZ)
+      {
+         covariance[0] = aCovar00;
+         covariance[1] = covariance[3] = aCovar01;
+         covariance[2] = covariance[6] = aCovar02;
+         covariance[4] = aCovar11;
+         covariance[5] = covariance[7] = aCovar12;
+         covariance[8] = aCovar22;
+      }
+      //> Constructor taking point and upper triangular portion of covariance matrix.
+      //  Elements are assigned to covariance matrix assuming symmetry.
+      //<
+  
+      //***
+      //> The 3 x 3 covariance matrix for the ECEF coordinate is stored in a one dimensional
+      // array.  Elements can either be accessed directly from that array, or via a 
+      // two dimensional lookup using the covar2d() methods below.  Note that using the
+      // "non-const" method allows assignment as well as retrieval.
+      // Ex.   To look up the variance in the Z direction of a coordinate "coord" and
+      // assign it to a variable A, the syntax would be :  
+      //
+      // Using one dimensional access : double A = coord.covariance[8];
+      // Using two dimensional access : double A = coord.covar2d(2,2);
+      //
+      // Similarly, to assign a value of 9.0 as the variance in the Z direction
+      // for "coord", the syntax would be :  
+      //
+      // Using one dimensional access : coord.covariance[8] = 9.0;
+      // Using two dimensional access : coord.covar2d(2,2) = 9.0;
+      //<
+      //***
       double covariance[9];
+      double  covar2d(unsigned int l, unsigned int s) const { return covariance[3*l + s]; }
+      double& covar2d(unsigned int l, unsigned int s)       { return covariance[3*l + s]; } 
    };
 
    //***
@@ -211,8 +369,36 @@ namespace csm
       EcefVector() : x(0.0), y(0.0), z(0.0) {}
       EcefVector(double aX, double aY, double aZ) : x(aX), y(aY), z(aZ) {}
    };
+   
+   //***
+   // STRUCT: EcefLocus
+   // > The EcefLocus structure contains a point and a direction in Earth
+   //   Centered Earth Fixed space.
+   //<
+   //***
+   struct EcefLocus
+   {
+   public:
+      EcefCoord  point;
+      EcefVector direction;
 
+      EcefLocus() : point() , direction() {}
+
+      EcefLocus(const EcefCoord& argPoint,
+                const EcefVector& argDirection)
+         :
+            point     (argPoint),
+            direction (argDirection)
+      {}
+
+      EcefLocus(double ptX , double ptY , double ptZ,
+                double dirX, double dirY, double dirZ)
+         :
+            point    (ptX, ptY, ptZ),
+            direction(dirX,dirY,dirZ)
+      {}
+   };
+             
 } // namespace csm
 
 #endif
-
