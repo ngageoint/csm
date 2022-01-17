ffdf9cbe061e119856563c0b6110d9695309d2ec
roseej <eugenerose4@gmail.com>
2022-01-17 10:35:06 -0500

    Add files via upload

---
+++ WGS84ReferenceFrame.cpp
@@ -0,0 +1,59 @@
+//####################################################################
+//
+//    FILENAME:          WGS84ReferenceFrame.cpp
+//
+//    CLASSIFICATION:    Unclassified
+//
+//    DESCRIPTION:
+//
+//    Implementation for WGS84 reference frame realization and epoch.
+//
+//    LIMITATIONS:       None
+//
+//
+//    SOFTWARE HISTORY:
+//     Date          Author    Comment
+//     -----------   ------    -------
+//     31-December-2021   Eugene Rose CCB Change
+//
+//    NOTES: 
+//    As of writing this, these are the WGS84 realizations to date.
+//    WGS84 ORIGINAL 1984.0
+//    G730           1994.0
+//    G873           1997.0
+//    G1150          2001.0
+//    G1674          2005.0
+//    G1762          2005.0
+//
+//###################################################################
+#define CSM_LIBRARY
+
+#include "csm.h"
+#include "WGS84ReferenceFrame.h"
+
+namespace csm
+{
+	//****************************************************************************
+	// WGS84ReferenceFrame::setDefinition()
+	//****************************************************************************
+	void WGS84ReferenceFrame::setDefinition(std::string name, const float epoch)
+	{
+		m_realization = realizationDefinition(name, epoch);
+	}
+
+	//****************************************************************************
+	// WGS84ReferenceFrame::setDefinition()
+	//****************************************************************************
+	void WGS84ReferenceFrame::setDefinition(const realizationDefinition& rd)
+	{
+		m_realization = rd;
+	}
+
+	//****************************************************************************
+	// WGS84ReferenceFrame::getDefinition()
+	//****************************************************************************
+	csm::WGS84ReferenceFrame::realizationDefinition WGS84ReferenceFrame::getDefinition() const
+	{
+		return m_realization;
+	}
+}
