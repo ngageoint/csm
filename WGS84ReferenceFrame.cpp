38f1c1a6a620d126c34263335763abf993d81ccb
roseej <eugenerose4@gmail.com>
2022-01-28 16:12:27 -0500

    Update WGS84ReferenceFrame.cpp
    fix previous update which inserted header file code into .cpp file.
    This addresses Scott's comments to the .cpp file.

--- WGS84ReferenceFrame.cpp
+++ WGS84ReferenceFrame.cpp
@@ -1,12 +1,12 @@
 //####################################################################
 //
-//    FILENAME:          WGS84ReferenceFrame.h
+//    FILENAME:          WGS84ReferenceFrame.cpp
 //
 //    CLASSIFICATION:    Unclassified
 //
 //    DESCRIPTION:
 //
-//    Header for WGS84 reference frame realization and epoch.
+//    Implementation for WGS84 reference frame realization and epoch.
 //
 //    LIMITATIONS:       None
 //
@@ -26,67 +26,34 @@
 //    G1762          2005.0
 //
 //###################################################################
-
-#ifndef __CSM_WGS84REFERENCEFRAME_H
-#define __CSM_WGS84REFERENCEFRAME_H
+#define CSM_LIBRARY
 
 #include "csm.h"
-#include <string>
-
-#define WGS84_ORIGINAL "WGS84 ORIGINAL"
-#define WGS84_G730 "G730"
-#define WGS84_G873 "G873"
-#define WGS84_G1150 "G1150"
-#define WGS84_G1674 "G1674"
-#define WGS84_G1762 "G1762"
+#include "WGS84ReferenceFrame.h"
 
 namespace csm
 {
-
-class CSM_EXPORT_API WGS84ReferenceFrame
+//****************************************************************************
+// WGS84ReferenceFrame::setDefinition()
+//****************************************************************************
+void WGS84ReferenceFrame::setDefinition(const std::string &name, const float epoch)
 {
-public:
-        
-    struct CSM_EXPORT_API RealizationDefinition 
-    {
-    public:
-        RealizationDefinition() : m_name("WGS84_ORIGINAL"), m_epoch(1984.0) {};
-        RealizationDefinition(const std::string &name, float epoch) : m_name(name), m_epoch(epoch) {};
-        std::string m_name;
-        float m_epoch;
-    };
-    //> Realizations are defined by a string name and a float epoch which is in terms of years since 0AD.
-    // The default realization is the original one from 1984.0
-    //<
+    m_realization = RealizationDefinition(name, epoch);
+}
 
-    WGS84ReferenceFrame() {};
-    //> construct default realization
-    //<
+//****************************************************************************
+// WGS84ReferenceFrame::setDefinition()
+//****************************************************************************
+void WGS84ReferenceFrame::setDefinition(const RealizationDefinition& rd)
+{
+    m_realization = rd;
+}
 
-    WGS84ReferenceFrame(const std::string &name, const float epoch) : m_realization(name,epoch) {};
-    //> construct custom realization
-    //<
-
-    void setDefinition(const std::string &name, const float epoch);
-    //> convenience to set the internal structure (it is public)
-    //<
-    
-    void setDefinition(const RealizationDefinition& rd);
-    //> convenience to set the internal structure (it is public)
-    //<
-
-    RealizationDefinition getDefinition() const;
-    //> convenience to get the internal structure (it is public)
-    //<
-        
-private:
-    RealizationDefinition m_realization;
-    //> the name and epoch of the realization are here
-    //<
-
-};
-
-} // namespace csm
-
-#endif // __CSM_WGS84REFERENCEFRAME_H
-
+//****************************************************************************
+// WGS84ReferenceFrame::getDefinition()
+//****************************************************************************
+csm::WGS84ReferenceFrame::RealizationDefinition WGS84ReferenceFrame::getDefinition() const
+{
+    return m_realization;
+}
+}
